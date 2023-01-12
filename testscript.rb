e3d25bc96736b4faa61ca13b3567d69116f91249
Shubham <shubhamveer228@gmail.com>
2023-01-13 01:43:11 +0530

    added testcript

---
+++ testscript.rb
@@ -0,0 +1,13 @@
+
+class Testscript < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/shubh018/testhb/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "7334ee517c4c7f7f4a860cee6c8addb6345572a3393176dd84575b2724b03ffb"
+  license ""
+
+  def install
+	bin.install "testscript"
+  end
+
+end
